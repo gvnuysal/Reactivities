d44f773ce1375d8203784c18826ff5cad3513521
gvnuysal <gvnuysal@gmail.com>
2020-02-10 08:32:05 +0300

    Initial

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+       public DbSet<Value> Values { get; set; }
+
+       /* protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(new Value
+            {
+                Id = 1,
+                Name = "Güven Uysal"
+            },new Value
+            {
+                Id = 2,
+                Name = "Tuncay Kurt"
+            },new Value
+            {
+                Id = 3,
+                Name = "Alparslan Şen"
+            },new Value
+            {
+                Id = 4,
+                Name = "Özgür Yurtseven"
+            });
+        }*/
+    }
+}
